1b662b6b953383db62827a5e615dc25bc3ccfe9e
Vladivarius <10048777+Vladivarius@users.noreply.github.com>
2020-12-27 19:18:32 +0100

    Create Regscan.cpp

---
+++ Enumerate and read Windows registry keys and their values/Regscan.cpp
@@ -0,0 +1,119 @@
+/* 
+Original non-working code posted by 8bitcartridge 
+here > https://stackoverflow.com/questions/6825555/enumerating-all-subkeys-and-values-in-a-windows-registry-key
+Code fixed by Vladivarius
+elamon.vlad@gmail.com
+27.12.2020.
+*/
+#include <Windows.h> //not sure if this is the only required thing to include
+
+using namespace std;
+LSTATUS result;
+BYTE databuffer[1024];
+
+void EnumerateValues(HKEY hKey, DWORD numValues)
+{
+	cout << "\nKeys found: " << numValues;
+	DWORD dwIndex = 0;
+	LPSTR valueName = new CHAR[64];
+	DWORD valNameLen;
+	DWORD dataType;
+	BYTE* data = databuffer;
+	DWORD dataSize = 1024;
+
+	for (int i = 0; i < numValues; i++)
+	{
+		result = 
+		RegEnumValue(hKey,
+			dwIndex,
+			valueName,
+			&valNameLen,
+			NULL,
+			&dataType,
+			data,
+			&dataSize);
+
+		if (result != ERROR_SUCCESS) {
+			cout << "\nError RegEnumValue > " << result;
+			return;
+		}
+		cout << "\nValue size: " << dataSize << "\nValue data: " << data;
+		dwIndex++;
+	}
+}
+
+void EnumerateSubKeys(HKEY RootKey, char* subKey, unsigned int tabs = 0)
+{
+	HKEY hKey;
+	DWORD cSubKeys;        //Used to store the number of Subkeys
+	DWORD maxSubkeyLen;    //Longest Subkey name length
+	DWORD cValues;        //Used to store the number of Subkeys
+	DWORD maxValueLen;    //Longest Subkey name length
+	DWORD retCode;        //Return values of calls
+
+	result =
+	RegOpenKeyEx(RootKey, subKey, 0, KEY_QUERY_VALUE | KEY_ENUMERATE_SUB_KEYS | KEY_READ /*KEY_ALL_ACCESS*/, &hKey);
+	if (result != ERROR_SUCCESS) {
+		cout << "\nError RegOpenKeyEx > " << result;
+		return;
+	}
+
+	result = 
+	RegQueryInfoKey(hKey,          // key handle
+		NULL,            // buffer for class name
+		NULL,            // size of class string
+		NULL,            // reserved
+		&cSubKeys,        // number of subkeys
+		&maxSubkeyLen,    // longest subkey length
+		NULL,            // longest class string 
+		&cValues,        // number of values for this key 
+		&maxValueLen,    // longest value name 
+		NULL,            // longest value data 
+		NULL,            // security descriptor 
+		NULL);            // last write time
+
+	if (result != ERROR_SUCCESS) {
+		cout << "\nError RegQueryInfoKey > " << result;
+		return;
+	}
+
+	if (cSubKeys > 0)
+	{
+		char currentSubkey[MAX_PATH];
+
+		for (int i = 0; i < cSubKeys; i++) {
+			DWORD currentSubLen = MAX_PATH;
+
+			retCode = RegEnumKeyEx(hKey,    // Handle to an open/predefined key
+				i,                // Index of the subkey to retrieve.
+				currentSubkey,            // buffer to receives the name of the subkey
+				&currentSubLen,            // size of that buffer
+				NULL,                // Reserved
+				NULL,                // buffer for class string 
+				NULL,                // size of that buffer
+				NULL);                // last write time
+
+			if (retCode == ERROR_SUCCESS)
+			{
+				for (int i = 0; i < tabs; i++)
+					printf("\t");
+				printf("(%d) %s\n", i + 1, currentSubkey);
+
+				char* subKeyPath = new char[currentSubLen + strlen(subKey)];
+				sprintf(subKeyPath, "%s\\%s", subKey, currentSubkey);
+				EnumerateSubKeys(RootKey, subKeyPath, (tabs + 1));
+			}
+		}
+	}
+	else
+	{
+		EnumerateValues(hKey, cValues);
+	}
+
+	RegCloseKey(hKey);
+}
+
+int main(){
+  //Check if a device is plugged in, for example Arduino Uno.
+  EnumerateSubKeys(HKEY_LOCAL_MACHINE, "HARDWARE\\DEVICEMAP\\SERIALCOMM");
+}
